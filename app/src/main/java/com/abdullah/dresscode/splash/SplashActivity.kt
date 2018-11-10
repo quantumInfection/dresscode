4ac01b582045e2652133ec86e0017b8b07b907be
Abdullah Aftab <a13dullah.a@gmail.com>
2018-11-10 22:04:38 +0500

    Add SplashActivityy

--- app/src/main/java/com/abdullah/dresscode/splash/SplashActivity.kt
+++ app/src/main/java/com/abdullah/dresscode/splash/SplashActivity.kt
@@ -1,6 +1,15 @@
 package com.abdullah.dresscode.splash
 
+import android.os.Bundle
 import com.abdullah.dresscode.base.BaseActivity
 
 class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.checkLoginState()
+    }
+
+    private fun checkLoginState() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
